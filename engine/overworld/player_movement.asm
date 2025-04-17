7b3b905881a0b1fed47436b2d53a4b5fafddb90a
redlambert <alecwestlambert@gmail.com>
2025-04-16 19:35:16 -0500

    Update player_movement.asm
    UpdateSprites (no underscore)

--- engine/overworld/player_movement.asm
+++ engine/overworld/player_movement.asm
@@ -818,7 +818,7 @@ ENDM
 	push bc
 	ld a, PLAYER_RUN
 	ld [wPlayerState], a
-	call UpdatePlayerSprite
+	call UpdateSprites
 	pop bc
 	ret
 
@@ -826,7 +826,7 @@ ENDM
 	push bc
 	ld a, PLAYER_NORMAL
 	ld [wPlayerState], a
-	call UpdatePlayerSprite
+	call UpdateSprites
 	pop bc
 	ret
 
