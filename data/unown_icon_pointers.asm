446fd0af5f66cd1cfad5fce6c839fe300a252b55
redlambert <alecwestlambert@gmail.com>
2025-04-19 12:42:49 -0500

    Create unown_icon_pointers.asm

---
+++ data/unown_icon_pointers.asm
@@ -0,0 +1,30 @@
++UnownIconPointers:
++; entries correspond to ICON_* constants (see constants/icon_constants.asm)
+
++	dw NullIcon
++	dw UnownAIcon
++	dw UnownBIcon
++	dw UnownCIcon
++	dw UnownDIcon
++	dw UnownEIcon
++	dw UnownFIcon
++	dw UnownGIcon
++	dw UnownHIcon
++	dw UnownIIcon
++	dw UnownJIcon
++	dw UnownKIcon
++	dw UnownLIcon
++	dw UnownMIcon
++	dw UnownNIcon
++	dw UnownOIcon
++	dw UnownPIcon
++	dw UnownQIcon
++	dw UnownRIcon
++	dw UnownSIcon
++	dw UnownTIcon
++	dw UnownUIcon
++	dw UnownVIcon
++	dw UnownWIcon
++	dw UnownXIcon
++	dw UnownYIcon
++	dw UnownZIcon
